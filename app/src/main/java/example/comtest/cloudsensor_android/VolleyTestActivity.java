fe7410bbfc3a868069597cc13028cb2e276227b4
Laney47 <Laney47@users.noreply.github.com>
2017-05-29 13:19:13 +0200

    Post object med arrays fungerar

--- app/src/main/java/example/comtest/cloudsensor_android/VolleyTestActivity.java
+++ app/src/main/java/example/comtest/cloudsensor_android/VolleyTestActivity.java
@@ -16,6 +16,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -33,9 +34,9 @@ public class VolleyTestActivity extends AppCompatActivity {
     private Button postButton;
     private String id;
     private String owner = "Axel";
-    //private String[] x;
-    //private String[] y;
-    //private String[] z;
+    private  double[] x ={1.0,2.0};
+    private double[] y ={1.0,2.0};
+    private double[] z ={1.0,2.0};
 
     @Override
     public final void onCreate(Bundle savedInstanceState) {
@@ -56,7 +57,7 @@ public class VolleyTestActivity extends AppCompatActivity {
         postButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                postToServer(owner,null,null,null);
+                postToServer(owner,x,y,z);
             }
         });
 
@@ -88,16 +89,22 @@ public class VolleyTestActivity extends AppCompatActivity {
     }
 
 
-        public void postToServer( final String owner, final String[] x, final String[] y, final String[] z){
+        public void postToServer( final String owner, final double[] x, final double[] y, final double[] z){
             String URL = "http://datacollectapi20170528075302.azurewebsites.net/api/Accelerometer/PostCollection";
             RequestQueue requestQueue = Volley.newRequestQueue(this);
             try {
-                HashMap<String, String> params = new HashMap<String, String>();
-
-                params.put("owner", owner.toString());
+                JSONObject jsObj = new JSONObject();
 
 
-                JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, URL, new JSONObject(params),
+
+                jsObj.put("owner", owner);
+                JSONArray xList = new JSONArray(x);
+                JSONArray yList = new JSONArray(y);
+                JSONArray zList = new JSONArray(z);
+                jsObj.put("x", xList);
+                jsObj.put("y", yList);
+                jsObj.put("z", zList);
+                JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, URL, jsObj,
                         new Response.Listener<JSONObject>() {
                             @Override
                             public void onResponse(JSONObject response) {
